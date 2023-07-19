24bb43beba950ee1241c0f3f2903f9e388bbe94d
aaru2599 <arvindmandre2@gmail.com>
2023-07-19 21:01:45 +0530

    Added Algo and comments

--- 01.longestWord.js
+++ 01.longestWord.js
@@ -1,15 +1,24 @@
+// Program to find longestWord in String 
+// Step1: We have to convert this string into seperated word for that 
+// we have to use split method
+// Step2:after that we will create one empty variable
+// Step3:After that we will check the length of every word with empty 
+// variable if it will greater then that then we will strore that word in 
+// empty variable
+
+
 function longestWordInString(input) {
     let longestWord = "";
+    // breaking the string into seperated array of word
     let newInput = input.split(" ");
-  
+    // iterating every element with for loop
     for (let i = 0; i < newInput.length; i++) {
-      if (newInput[i].length > longestWord.length) {
-        longestWord = newInput[i];
-      }
+        if (newInput[i].length > longestWord.length) {
+            longestWord = newInput[i];
+        }
     }
-  
-    return longestWord;
-  }
-  
-  console.log(longestWordInString("My Name is Arvind and I'm from Chhindwara"));
-  
+
+    return `Longest Word is:- ${longestWord}`;
+}
+
+console.log(longestWordInString("My Name is Arvind and I'm from Chhindwara"));
